d916ec7d3e3637ed36fad4fc92dd645a2b7b3cf7
Eugene <glushin.evgeniy@gmail.com>
2019-08-26 11:33:57 +0300

    Load test data.

---
+++ ElasticsearchSupplyCollector/ElasticsearchDataLoader/SampleDataProvider.cs
@@ -0,0 +1,28 @@
+﻿using ElasticsearchDataLoader.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ElasticsearchDataLoader
+{
+    public class SampleDataProvider
+    {
+        public List<Person> GetPeople(int number)
+        {
+            var list = new List<Person>(number);
+
+            var noLastName = new Person() { Id = Guid.NewGuid(), FirstName = "Eugene" };
+            var deleted = new Person() { Id = Guid.NewGuid(), FirstName = "Eugene (deleted)", IsDeleted = true };
+
+            list.Add(noLastName);
+            list.Add(deleted);
+
+            for (int i = 0; i < number; i++)
+            {
+                var person = new Person(i, 2, 3);
+                list.Add(person);
+            }
+
+            return list;
+        }
+    }
+}
